e288a47812c1705712f1a3cff7b3e1216eb88073
Brett Nguyen <komoru8@gmail.com>
2025-03-14 22:05:27 -0400

    f

--- web_scrap.py
+++ web_scrap.py
@@ -25,6 +25,8 @@ chrome_options.add_argument("--headless")  # Run Chrome in headless mode
 chrome_options.add_argument("--no-sandbox")  # Required for some environments
 chrome_options.add_argument("--disable-dev-shm-usage")  # Prevents crashes in Docker/Linux
 chrome_options.add_argument("--window-size=1920x1080")  # Optional, set window size
+chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36")
+
 prefs = {
     "safebrowsing.enabled": True
 }
@@ -37,7 +39,7 @@ driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
 try:
     # Open the website
     driver.get("https://www.cnn.com/markets/premarkets")
-    time.sleep(10)
+    time.sleep(30)
     # Wait for the element to be visible (up to 10 seconds)
     element = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, element_css))
